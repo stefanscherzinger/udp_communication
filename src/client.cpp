a6951da2eae3b1a1762ca3ed758fa5c9952d16dd
Stefan Scherzinger <scherzin@fzi.de>
2023-08-20 13:44:27 +0200

    Send data at a specific rate
    We want to add *reliability* and *ordering* later.

--- src/client.cpp
+++ src/client.cpp
@@ -1,4 +1,7 @@
 #include "boost/asio.hpp"
+#include <chrono>
+#include <thread>
+
 using namespace boost::asio;
 
 int main()
@@ -13,8 +16,14 @@ int main()
   remote_endpoint = ip::udp::endpoint(ip::address::from_string("127.0.0.1"), port);
 
   boost::system::error_code err;
-  std::string data = "Arbitrary test data";
-  socket.send_to(buffer(data, data.size()), remote_endpoint, 0, err);
+
+  constexpr double rate = 3.0;  // Hz
+  while (true)
+  {
+    std::string data = "Arbitrary test data";
+    socket.send_to(buffer(data, data.size()), remote_endpoint, 0, err);
+    std::this_thread::sleep_for(std::chrono::duration<double>(1.0 / rate));
+  }
 
   socket.close();
 
